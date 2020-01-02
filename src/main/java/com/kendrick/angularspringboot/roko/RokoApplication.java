99876647eb5a6adb97441ef2db6ab4664ee24e4c
Kendrick <kenwilli@redhat.com>
2020-01-02 12:56:57 -0600

    update

---
+++ src/main/java/com/kendrick/angularspringboot/roko/RokoApplication.java
@@ -0,0 +1,13 @@
+package com.kendrick.angularspringboot.roko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kendrick/angularspringboot/roko/RokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kendrick.angularspringboot.roko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
